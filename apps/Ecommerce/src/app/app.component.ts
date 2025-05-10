e8ff40e7c4f4e04ce261dfeb86b102baa739e3c1
LencyBhalodiya <worklency@gmail.com>
2025-05-10 18:50:13 +0530

    common lib declared

--- apps/Ecommerce/src/app/app.component.ts
+++ apps/Ecommerce/src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { UtilService } from '@angular-monorepo/shared-utils';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   imports: [RouterModule],
+  providers: [UtilService],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Ecommerce';
+  constructor(private _utilService: UtilService) { }
+  title: any = 'Ecommerce';
+
+  ngOnInit() {
+    this.title = this._utilService.test();
+  }
 }
--- libs/shared-utils/src/index.ts
+++ libs/shared-utils/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/shared-utils/shared-utils.component';
+export * from './lib/shared-utils/services/util.service';
