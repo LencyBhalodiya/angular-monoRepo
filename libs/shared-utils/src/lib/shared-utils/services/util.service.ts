07f75308dfa04bf86f495dcd86ad6e465bbe4c1b
LencyBhalodiya <worklency@gmail.com>
2025-05-24 17:46:39 +0530

    removed unused

--- libs/shared-utils/src/lib/shared-utils/services/util.service.ts
+++ libs/shared-utils/src/lib/shared-utils/services/util.service.ts
@@ -9,29 +9,43 @@ import { catchError, EMPTY, Observable } from "rxjs";
 export class UtilService {
 
   http = inject(HttpClient);
-  public test() {
-    return 12323;
-  }
 
+  /**
+   * Performs an HTTP GET request.
+   *
+   * @param url The URL to be requested.
+   * @param headers The headers of the request. Defaults to an empty object.
+   * @param ErrorCallback The callback function that will be called if an error occurs during the request. Defaults to null.
+   * @returns The observable that represents the request.
+   */
   getHttp(url: string, headers: Obj = {}, ErrorCallback: any = null): Observable<any> {
     const request$ = this.http.get(url, headers)
 
     return request$.pipe(
       catchError((err) => {
         ErrorCallback && ErrorCallback(err);
-        console.error('An error occurred:', err);
+        console.error('An error occurred to Get Http:', err);
         return EMPTY;
       })
     );
   }
 
+  /**
+   * Posts an HTTP request.
+   *
+   * @param url The URL to be posted to.
+   * @param body The object to be posted. Defaults to an empty object.
+   * @param headers The headers of the request. Defaults to an empty object.
+   * @param ErrorCallback The callback function that will be called if an error occurs during the request. Defaults to null.
+   * @returns The observable that represents the request.
+   */
   postHttp(url: string, body: Obj = {}, headers: Obj = {}, ErrorCallback: any = null): Observable<any> {
-    const request$ = this.http.post(url, body, headers)
+    const request$ = this.http.post(url, body, headers);
 
     return request$.pipe(
       catchError((err) => {
         ErrorCallback && ErrorCallback(err);
-        console.error('An error occurred:', err);
+        console.error('An error occurred to Post Http:', err);
         return EMPTY;
       })
     );
